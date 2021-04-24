05f302d3aac5a6ddd8d3031502fc6eb33aeb9d44
Kevin <kevin.cruz@hetic.net>
2021-04-25 01:07:38 +0200

    begin

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Skyrock</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+<?php include_once("nav_home.php");?>
+</header>
+<main>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing 
+elit. Ex eum sunt autem ullam enim laboriosam dolor quam,
+excepturi optio unde odit? Aspernatur dolore consequatur 
+reprehenderit id necessitatibus quae minima at.
+</p>
+</main>
+</body>
+</html>
---
+++ nav_home.php
@@ -0,0 +1,7 @@
+
+<nav>
+<a href="index.php">Home</a>
+<a href="">Biographie</a>
+<a href="">Blog</a>
+<a href="login.php">Login</a>
+</nav>
